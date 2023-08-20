2c8287c600b109ef4ae9b4c720e8776a93216983
lvtoness <88632356+Wizardkkj@users.noreply.github.com>
2023-08-20 17:08:00 -0300

    Update byiluv.lua

--- byiluv.lua
+++ byiluv.lua
@@ -278,3 +278,120 @@ rootPart.CFrame = game:GetService("Workspace").Challenge.BalanceBall.Finish.CFra
 
   	end    
 })
+
+
+Tab4:AddButton({
+	Name = "Head Expander Pig",
+	Callback = function()
+          local part = local part = game:GetService("Workspace").Challenge.PiggyPlaytime.PigZel.Union
+
+-- Função para expandir a hitbox
+local function expandHitbox(object, amount)
+    local originalSize = object.Size
+    local newSize = originalSize + Vector3.new(amount, amount, amount)
+    object.Size = newSize
+end
+
+-- Função para desenhar a hitbox
+local function drawHitbox(object)
+    local box = Instance.new("SelectionBox")
+    box.Parent = object
+    box.Adornee = object
+end
+
+-- Chame a função para expandir a hitbox do objeto em 10 unidades
+expandHitbox(part, 10) -- Aumentado para 10 unidades
+
+-- Chame a função para desenhar a hitbox do objeto
+drawHitbox(part)
+
+-- Função para expandir a hitbox
+local function expandHitbox(object, amount)
+    local originalSize = object.Size
+    local newSize = originalSize + Vector3.new(amount, amount, amount)
+    object.Size = newSize
+end
+
+-- Função para desenhar a hitbox
+local function drawHitbox(object)
+    local box = Instance.new("SelectionBox")
+    box.Parent = object
+    box.Adornee = object
+end
+
+-- Função para lidar com toques no objeto
+local function onTouched(otherPart)
+    local character = otherPart.Parent
+    if character and character:FindFirstChild("Humanoid") then
+        local humanoid = character:FindFirstChild("Humanoid")
+        -- Aqui você pode adicionar a lógica para lidar com a interação
+        print(character.Name .. " tocou no objeto com hitbox expandida!")
+    end
+end
+
+-- Configure a função onTouched para o objeto
+part.Touched:Connect(onTouched)
+
+-- Chame a função para expandir a hitbox do objeto em 10 unidades
+expandHitbox(part, 10)
+
+-- Chame a função para desenhar a hitbox do objeto
+drawHitbox(part)
+
+      		OrionLib:MakeNotification({
+	Name = "Expanded",
+	Content = "Expanded",
+	Image = "rbxassetid://4483345998",
+	Time = 2
+})
+
+  	end    
+})
+
+Tab4:AddButton({
+	Name = "Mask Expander Hitbox",
+	Callback = function()
+          local part = game:GetService("Workspace").Finish.MaskTheGas.Parts.Part
+
+-- Função para expandir a hitbox
+local function expandHitbox(object, amount)
+    local originalSize = object.Size
+    local newSize = originalSize + Vector3.new(amount, amount, amount)
+    object.Size = newSize
+end
+
+-- Função para desenhar a hitbox
+local function drawHitbox(object)
+    local box = Instance.new("SelectionBox")
+    box.Parent = object
+    box.Adornee = object
+end
+
+-- Função para lidar com toques no objeto
+local function onTouched(otherPart)
+    local character = otherPart.Parent
+    if character and character:FindFirstChild("Humanoid") then
+        local humanoid = character:FindFirstChild("Humanoid")
+        -- Aqui você pode adicionar a lógica para lidar com a interação
+        print(character.Name .. " tocou no objeto com hitbox expandida!")
+    end
+end
+
+-- Configure a função onTouched para o objeto
+part.Touched:Connect(onTouched)
+
+-- Chame a função para expandir a hitbox do objeto em 10 unidades
+expandHitbox(part, 10)
+
+-- Chame a função para desenhar a hitbox do objeto
+drawHitbox(part)
+
+      		OrionLib:MakeNotification({
+	Name = "Finished the Challenge",
+	Content = "You win :)",
+	Image = "rbxassetid://4483345998",
+	Time = 2
+})
+
+  	end    
+})
